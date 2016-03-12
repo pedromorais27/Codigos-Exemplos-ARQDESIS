c4ad88b2ed98768a7b434739671305a19d6e47b3
Administrador <Administrador@BUTNTBLI068193>
2016-03-11 22:02:11 -0300

    Aula 3 arquitetura e desenvolvimento de sistemas

---
+++ arqdesis/src/model/ClienteTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClienteTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
